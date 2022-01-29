11bbd246d13306f821d98d71f3098e7e18a95a7b
ah0528 <0528x0x@gmail.com>
2022-01-29 12:04:25 +0900

    bj_10828

---
+++ sol_1000-9999/10828_스택.py
@@ -0,0 +1,37 @@
+# 정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+
+# 명령은 총 다섯 가지이다.
+
+# push X: 정수 X를 스택에 넣는 연산이다.
+# pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# size: 스택에 들어있는 정수의 개수를 출력한다.
+# empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+# top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+stack = []
+
+n = int(input())
+for _ in range(n):
+    inp =input().split()
+    # print(inp)
+    if (inp[0] == "push"):
+        stack.append(inp[1])
+
+    elif (inp[0] == "pop"):
+        if (len(stack) == 0):
+            print(-1)
+        else:
+            data = stack.pop()
+            print(data)
+
+    elif (inp[0] == "size"):
+        print(len(stack))
+
+    elif (inp[0] == "empty"):
+        if (len(stack) == 0):
+            print(1)
+        else:
+            print(0)
+
+    elif (inp[0] == "top"):
+        print(stack[-1])
