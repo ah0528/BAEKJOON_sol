5c564e792f27cd7eb1950c561841f49a8c099f0e
ah0528 <0528x0x@gmail.com>
2022-02-05 15:48:30 +0900

    bj_11279

---
+++ sol_1000-9999/11279_최대힙.py
@@ -0,0 +1,20 @@
+# 11279_최대힙
+import sys
+# from queue import PriorityQueue
+import heapq
+
+input = sys.stdin.readline
+# queue = PriorityQueue()
+heap = []
+n = int(input())
+
+for _ in range(n):
+    inp = int(input())
+
+    if (inp != 0):
+        heapq.heappush(heap, -inp)
+    else:
+        if (len(heap) == 0):
+            print(0)
+        else:
+            print(-heapq.heappop(heap))
